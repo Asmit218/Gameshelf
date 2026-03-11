8a78ec584023e1db5013d1c211aaacf6c07b9c0a
Adiya Kumar <adityakumar20741@gmail.com>
2026-03-11 14:39:59 +0530

    adding frontend

---
+++ Client/src/pages/homepage.jsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">Welcome to GameShelf!</h1>
+        <p className="text-lg text-base-500 mb-8">Your personal game library manager</p>
+    </div>
+  )
+}
+
