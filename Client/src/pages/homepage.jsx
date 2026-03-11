0d0a1c9ac191cd5563afe095d541f21c066f84c9
Adiya Kumar <adityakumar20741@gmail.com>
2026-03-11 17:05:08 +0530

    adding minor frontend

--- Client/src/pages/homepage.jsx
+++ Client/src/pages/homepage.jsx
@@ -1,9 +1,8 @@
+import Navbar from "../components/Navbar";
 export default function Homepage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">Welcome to GameShelf!</h1>
-        <p className="text-lg text-base-500 mb-8">Your personal game library manager</p>
+    <div className="flex justify-center backdrop-blur-xs rounded-full m-5 shadow-2xl">
+        <Navbar />
     </div>
   )
 }
-
