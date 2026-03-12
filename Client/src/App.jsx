ecf9ebdc08a5a12687d4a4729dd50f7ba37dee9e
Asmit218 <asmitsrivastava21@gmail.com>
2026-03-12 16:06:07 +0530

    made finisher compatible with browser theme

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,43 +6,64 @@ import { useEffect } from "react";
 function App() {
 
   useEffect(() => {
-    if (window.FinisherHeader) {
-      new window.FinisherHeader({
-        "count": 100,
-        "size": {
-          "min": 1,
-          "max": 3,
-          "pulse": 0
-        },
-        "speed": {
-          "x": {
-            "min": 0,
-            "max": 0.4
+
+    const media = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const applyTheme = () => {
+      let light, lighta, lightb, lightc;
+      if (media.matches) {
+        light = "#000000";
+        lighta = "#fbfcca";
+        lightb = "#d7f3fe";
+        lightc = "#ffd0a7";
+      } else {
+        light = "#ffffff";
+        lighta = "#4f4f4e";
+        lightb = "#3e494d";
+        lightc = "#967e6a";
+      }
+
+      if (window.FinisherHeader) {
+        new window.FinisherHeader({
+          "count": 100,
+          "size": {
+            "min": 1,
+            "max": 3,
+            "pulse": 0
           },
-          "y": {
-            "min": 0,
-            "max": 0.6
-          }
-        },
-        "colors": {
-          "background": "{base-100}",
-          "particles": [
-            "#fbfcca",
-            "#d7f3fe",
-            "#ffd0a7"
+          "speed": {
+            "x": {
+              "min": 0,
+              "max": 0.4
+            },
+            "y": {
+              "min": 0,
+              "max": 0.6
+            }
+          },
+          "colors": {
+            "background": light,
+            "particles": [
+              lighta,
+              lightb,
+              lightc
+            ]
+          },
+          "blending": "overlay",
+          "opacity": {
+            "center": 1,
+            "edge": 1
+          },
+          "skew": 0,
+          "shapes": [
+            "c"
           ]
-        },
-        "blending": "overlay",
-        "opacity": {
-          "center": 1,
-          "edge": 1
-        },
-        "skew": 0,
-        "shapes": [
-          "c"
-        ]
-      });
+        });
+      }
     }
+    applyTheme();
+    media.addEventListener("change",applyTheme);
+    return () => media.removeEventListener("change",applyTheme);
   }, []);
 
 
